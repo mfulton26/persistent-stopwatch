0ca7ecc6caeb7d4719086f3d4ebf07574d3aa205
Mark Fulton <mark.fulton@asurion.com>
2019-09-14 09:18:13 -0500

    sw.js: cache icons too

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
 self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open("sw-cache").then(function(cache) {
-      return cache.add("index.html");
-    })
-  );
+  async function installResources() {
+    const urlsToCache = [
+      "index.html",
+      "icons/baseline_timer_black_24px.svg",
+      "icons/baseline_timer_black_18.png",
+      "icons/baseline_timer_black_24.png",
+      "icons/baseline_timer_black_36.png",
+      "icons/baseline_timer_black_48.png"
+    ];
+    const cache = await caches.open("sw-cache");
+    await cache.addAll(urlsToCache);
+  }
+  event.waitUntil(installResources());
 });
 
 self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  async function fetchResource(request) {
+    const responseFromCache = await caches.match(event.request);
+    return responseFromCache || fetch(request);
+  }
+  event.respondWith(fetchResource(event.request));
 });
